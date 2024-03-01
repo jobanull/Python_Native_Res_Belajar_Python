3aec17c3888bc1abb5f3d4555a7694d5cb7128d6
Joban <jikhdan@gmail.com>
2024-03-01 17:35:37 +0700

    Data Collection

--- data.py
+++ data.py
@@ -157,3 +157,180 @@ print("Nama saya {}".format(name))
 Output: 
 Nama saya Ikhdan
 """
+
+# Tipe Data Collection
+# List
+# List merupakan jenis kumpulan data terurut (ordered sequence) dan salah satu tipe data yang sering digunakan pada Python
+
+x = [1, 2.2, 'Dicoding']
+print(type(x))
+
+"""
+Output: 
+<class "list">
+"""
+
+# Anda dapat mengakses setiap indeks pada list dengan metode indexing. 
+
+x = [1, 'Dicoding', True, 1.0]
+
+print(x[2])
+
+""" 
+Output:
+True
+"""
+
+# List bersifat mutable (dapat diubah)
+x = [1, 2.2, 'Dicoding']
+x[0] = 'Indonesia'
+print(x)
+
+"""
+Output:
+['Indonesia', 2.2, 'Dicoding']
+"""
+
+x = ["laptop", "monitor", "mouse", "mousepad", "keyboard", "webcam", "microphone"]
+
+print(x[0])
+print(x[2])
+print(x[-1])
+print(x[-3])
+
+
+"""
+Output:
+laptop
+mouse
+microphone
+keyboard
+"""
+
+
+x = ["laptop", "monitor", "mouse", "mousepad", "keyboard", "webcam", "microphone"]
+
+print(x[0:5:2])
+print(x[1:])
+print(x[:3])
+
+"""
+Output:
+['laptop', 'mouse', 'keyboard']
+['monitor', 'mouse', 'mousepad', 'keyboard', 'webcam', 'microphone']
+['laptop', 'monitor', 'mouse']
+
+"""
+
+
+# Tuple
+# Tuple adalah jenis dari list yang tidak dapat diubah elemennya.
+x = (1, "Dicoding", 1+3j)
+print(type(x))
+
+"""
+Output:
+<class 'tuple'>
+"""
+
+# Tuple bersifat immutable yang artinya tidak dapat diubah.
+x = (5, 'program', 1+3j)
+# x[1] = 'Dicoding'
+
+"""
+Output:
+'tuple' object does not support item assignment
+"""
+
+# Set 
+# Set adalah kumpulan item bersifat unik, tanpa urutan (unordered collection), dan dapat diinisialisasikan dengan kurawal “{}” di mana setiap elemennya dipisahkan dengan koma.
+
+x = {1,2,7,2,3,13,3}
+# print(x[0])
+
+"""
+Output:
+'set' object is not subscriptable
+"""
+
+x = {1, 2, 7, 2, 3, 13, 3}
+print(x)
+print(type(x))
+
+"""
+Output:
+{1, 2, 3, 7, 13}
+<class 'set'>
+"""
+
+set1 = {1, 2, 3, 4, 5}
+set2 = {4, 5, 6, 7, 8}
+
+union = set1.union(set2)
+print("Union:", union)
+
+intersection = set1.intersection(set2)
+print("Intersection:", intersection)
+
+"""
+Output:
+Union: {1, 2, 3, 4, 5, 6, 7, 8}
+Intersection: {4, 5}
+
+"""
+
+# Dictionary
+# Dictionary pada Python merupakan kumpulan pasangan key-value yang bersifat tidak berurutan.
+
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+
+print(type(x))
+
+"""
+Output:
+<class 'dict'>
+"""
+
+# Kode akan menghasilkan error jika mencoba mengakses data pada dictionary dengan menggunakan metode indexing.
+
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+
+# print(x[0])
+
+""" 
+Output:
+KeyError: 0
+"""
+
+# yang benar
+
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+
+print(x ['name'])
+
+""" 
+Output:
+Perseus Evans
+"""
+
+# Menambah data pada Dictionary
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+x ['Job'] = "Web Developer"
+
+print(x)
+
+"""
+Output:
+{'name': 'Perseus Evans', 'age': 20, 'isMarried': False, 'Job': 'Web Developer'}
+"""
+
+# Menghapus data pada Dictionary
+x = { 'name': 'Perseus Evans', 'age': 20, 'isMarried': False }
+del x['isMarried']
+
+print(x)
+
+"""
+Output:
+{'name': 'Perseus Evans', 'age': 20}
+"""
