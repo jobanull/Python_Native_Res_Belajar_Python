8e6bc22ef330787f13b9df23bb1baa37e83c4d90
Joban <jikhdan@gmail.com>
2024-03-01 17:37:56 +0700

    Transformation Upper & Lower

---
+++ transformation.py
@@ -0,0 +1,19 @@
+# Upper
+kata = 'dicoding'
+kata = kata.upper()
+print(kata)
+
+"""
+Output:
+DICODING
+"""
+
+# Lower
+kata = 'DICODING'
+kata = kata.lower()
+print(kata)
+
+"""
+Output:
+dicoding
+"""
