1e53f5c47609b27790bc7a8686e5886363c86b0e
Joban <jikhdan@gmail.com>
2024-03-01 16:26:07 +0700

    Data Typing

---
+++ belajar.py
@@ -0,0 +1,19 @@
+# Python is Loosely Typed. 
+# Artinya, Anda tidak perlu mendeklarasikan tipe data variabel secara eksplisit.
+
+age = 17 
+salary = 30000.0
+
+print(type(age))
+print(type(salary))
+
+
+# Dynamic Typing
+# Artinya, Python adalah bahasa pemrograman yang hanya mengetahui tipe variabel saat program berjalan dan melakukan proses assignment. 
+# Hal ini memungkinkan kita untuk mengubah tipe data dari suatu variabel seiring berjalannya program.
+
+x = 6 
+print(type(x))
+
+x = "6"
+print(type(x))
